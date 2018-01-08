1e2e7ce2f67c0971901e00f528660d0c53470380
Marc Lavergne <mlavergn@gmail.com>
2018-01-08 09:48:48 -0800

    Add: C bridging playground

---
+++ CBridge.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// int array
+
+let array: [Int8] = [1, 2, 3, 4]
+
+array.withUnsafeBufferPointer { (ptr: UnsafeBufferPointer<Int8>) in
+    print(ptr.baseAddress! + 1)
+}
+
+// char array
+
+var cap = 10
+var ptr = UnsafeMutablePointer<CChar>.allocate(capacity: cap)
+var buf = UnsafeMutableBufferPointer(start: ptr, count: cap)
+_ = buf.initialize(from: repeatElement(0, count: cap))
+
+// set a value at index 5
+ptr[5] = 123
+
+// show me
+var i = 0
+for val in buf {
+    print(i, val)
+    i += 1
+}
+
+// cleanup refs
+ptr.deinitialize()
+// free
+ptr.deallocate(capacity: cap)
